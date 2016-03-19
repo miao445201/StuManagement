7dc465f5eb602eb521fcaa9caa6b01bf3f19d270
miao445201 <870291020@qq.com>
2016-03-19 12:02:05 +0800

    the first commit

---
+++ StuManagement/StuManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StuManagement
+//
+//  Created by fitfun on 16/3/19.
+//  Copyright © 2016年 miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
